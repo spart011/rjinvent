a58391ebde07cd26c7faf536bf43c6150f960ed2
spart011 <57788930+spart011@users.noreply.github.com>
2020-02-10 15:10:14 +0200

    Update home2.php

--- home2.php
+++ home2.php
@@ -121,6 +121,15 @@
 <script src="<?php echo base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
 <script src="<?php echo base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo base_url() ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="<?php echo base_url() ?>assets/js/scrolling-nav.js"></script>
+<script src="<?php echo base_url() ?>assets/js/scrolling-nav.js"></script><script type="text/javascript">
+      /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+      (function(d, m){
+        var kommunicateSettings = {"appId":"1ee0b5c30db547113bc587b1b19c8beb9","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+      })(document, window.kommunicate || {});
+  </script>
 </body>
 </html>
